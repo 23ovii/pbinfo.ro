9d27b58076fc969c13345183a14a10d753d5d827
ovii <168116208+23ovii@users.noreply.github.com>
2025-04-10 14:00:42 +0300

    solved

---
+++ 1633.cpp
@@ -0,0 +1,27 @@
+// 80pct
+#include<iostream>
+using namespace std;
+void dublare1(int &n) {
+    int invers;
+    invers = 0;
+    while (n > 0) {
+        int c;
+        c = n % 10;
+        n = n / 10;
+        invers = invers * 10 + c;
+    }  invers = invers * 10 + invers % 10;
+    n=0;
+    while (invers > 0) {
+        int c;
+        c = invers % 10;
+        invers = invers / 10;
+        n = n * 10 + c;
+    } 
+}
+
+int main() {
+    int n;
+    cin>>n;
+    dublare1(n);
+    cout<<n<<endl;
+}
---
+++ 21.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+void sum_cif(int n, int &s) {
+    s = 0;
+    while (n > 0) {
+        s = s + n % 10;
+        n = n / 10;
+    }
+}
