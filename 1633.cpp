9a7bb0741cab0edcff3baca7ffae8b95e42260ad
23ovii <danyovidiu21@gmail.com>
2025-04-11 20:14:36 +0300

    small changes

--- 1633.cpp
+++ 1633.cpp
@@ -1,18 +1,14 @@
-// 80/100 points.
+// 80pct
 #include<iostream>
 using namespace std;
 void dublare1(int &n) {
-    int invers;
-    invers = 0;
+    int invers=0,c;
     while (n > 0) {
-        int c;
         c = n % 10;
         n = n / 10;
         invers = invers * 10 + c;
-    }  invers = invers * 10 + invers % 10;
-    n=0;
+    }  invers = invers * 10 + c;
     while (invers > 0) {
-        int c;
         c = invers % 10;
         invers = invers / 10;
         n = n * 10 + c;
