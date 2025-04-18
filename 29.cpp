0c7e58655012fbf0ca6e77445dcdb41687a9c6b2
23ovii <danyovidiu21@gmail.com>
2025-04-18 15:10:11 +0300

    optimize prime number calculation in sub function

--- 29.cpp
+++ 29.cpp
@@ -1,25 +1,17 @@
-// 90/100 pct
 #include<iostream>
 using namespace std;
+int prim(int n) {
+    if (n < 2) return 0;
+    if (n % 2 == 0 && n > 2) return 0;
+    for (int d = 3; d * d <= n; d = d + 2)
+        if (n % d == 0) return 0;
+    return 1;
+}
 void sub(int n, int &a, int &b){
-    int d=0;
-    a=0; b=0;
-    for(int i=n-1; i>0; i--){
-            d=0;
-            for(int j=1; j<=i; j++){
-                if(i%j==0){
-                    d++;
-                }
-                if(d>2) break;
-            }
-            if(d==2){
-                if(a==0) a=i;
-                else {
-                    b=i;
-                    break;
-                }
-            }
-    }
+    a = n - 1;
+    while (!prim(a)) a--;
+    b = a-1;
+    while (!prim(b)) b--;
 }
 int main(){
     int n, a, b;
