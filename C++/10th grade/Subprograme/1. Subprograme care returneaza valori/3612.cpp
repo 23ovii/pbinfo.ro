db54f9d00eb27c308e168425d29f9d320e34bb68
23ovii <danyovidiu21@gmail.com>
2025-05-01 21:29:38 +0300

    solved

--- C++/10th grade/Subprograme/1. Subprograme care returneaza valori/3612.cpp
+++ C++/10th grade/Subprograme/1. Subprograme care returneaza valori/3612.cpp
@@ -1,29 +1,35 @@
 // not solved yet, pretty hard imma try it later
 #include<iostream>
 using namespace std;
-int divizori(int n){
+int divizori(int n) {
     int s = 0;
-    for (int i = 1; i <= n; i++)
-        if (n % i == 0)
+    for (int i = 1; i * i <= n; i++) {
+        if (n % i == 0) {
             s += i;
+            if (i != n / i) {
+                s += n / i;
+            }
+        }
+    }
     return s;
 }
 int kpn(int a, int b, int k){
-    int t = -1; int v[100];
-    for(int i = a; i <= b; i++){
-        if (divizori(i)%2 == 0 && i%2 == 0){
-            v[++t] = i;
-        } else if (divizori(i)%2 == 1 && i%2 == 1){
-            v[++t] = i;
-        }
+    while(k) {
+        int ver = divizori(a);
+        if(ver%2==0 && a%2==0){
+            k--;
+        } else if(ver%2==1 && a%2==1) {
+            k--;
+        } a++;
+    } a--; if(k == 0 && a<=b) {
+        return a;
+    } else {
+        return -1;
     }
-    return v[k];
 }
 int main(){
     int a, b, k;
-    cout << "a = "; cin >> a;
-    cout << "b = "; cin >> b;
-    cout << "k = "; cin >> k;
-    cout << "Elementul de rang " << k << " este " << kpn(a, b, k) << endl;
+    cin>>a>>b>>k;
+    cout<<kpn(a,b,k);
     return 0;
 }
