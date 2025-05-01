60418ad2fb494d0d8c5533cd81678605f6ee3b76
23ovii <danyovidiu21@gmail.com>
2025-05-01 21:54:49 +0300

    solved

--- C++/10th grade/Subprograme/1. Subprograme care returneaza valori/26.cpp
+++ C++/10th grade/Subprograme/1. Subprograme care returneaza valori/26.cpp
@@ -2,24 +2,36 @@
 #include<iostream>
 using namespace std;
 int cmmnr(int n) {
-    int v[11], cifre = 0;
-    while (n){
+    int v[11], cifre = 0, aux;
+    while (n) {
         v[++cifre] = n % 10;
         n /= 10;
-    } for (int i = 1; i < cifre; i++) {
+    }
+    for (int i = 1; i < cifre; i++) {
         for (int j = i + 1; j <= cifre; j++) {
             if (v[i] > v[j]) {
-                int aux;
                 aux = v[i];
                 v[i] = v[j];
                 v[j] = aux;
-                }
             }
         }
-    for (int i = 1; i <= cifre; i++)
-        n = n * 10 + v[i];
+    }
+    n = 0;
+    for (int i = 1; i <= cifre; i++) {
+        if (v[i] != 0) {
+            n = v[i];
+            v[i] = -1;
+            break;
+        }
+    }
+    for (int i = 1; i <= cifre; i++) {  
+        if (v[i] != -1) {  
+            n = n * 10 + v[i];
+        }
+    }
     return n;
 }
+
 int main(){
     int n;
     cin>>n;
