0ac2cb9e94240e8ce9de89ca66fcc487ab746f2b
23ovii <danyovidiu21@gmail.com>
2025-05-02 11:33:26 +0300

    solved

--- C++/10th grade/Subprograme/2. Subprograme care intorc valori prin parametrii/4190.cpp
+++ C++/10th grade/Subprograme/2. Subprograme care intorc valori prin parametrii/4190.cpp
@@ -1,16 +1,18 @@
-// 70/100 pct
 #include<iostream>
+#include<cmath>
 using namespace std;
 void patrate(int n, int &x, int &y) {
     x = 0; y = 0;
-    for(int i = 1; i <= n; i++) {
-        for(int j = 1; j <= n; j++) {
-            if(i * i * j * j == n) {
-                if(i >= 2 && j > i) {
-                    x = i;
-                    y = j;
-                    return;
-                }
+    int rad = (int)sqrt(n);
+    if (rad * rad != n) return;
+    for (int i = 2; i * i <= rad; i++) {
+        if (rad % i == 0) {
+            int a = i;
+            int b = rad / i;
+            if (a < b) {
+                x = a;
+                y = b;
+                return;
             }
         }
     }
