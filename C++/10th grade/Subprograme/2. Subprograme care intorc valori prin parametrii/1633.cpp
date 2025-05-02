e3e47e991657d32893ce71b933f0140343083861
23ovii <danyovidiu21@gmail.com>
2025-05-02 11:19:43 +0300

    solved

--- C++/10th grade/Subprograme/2. Subprograme care intorc valori prin parametrii/1633.cpp
+++ C++/10th grade/Subprograme/2. Subprograme care intorc valori prin parametrii/1633.cpp
@@ -1,20 +1,12 @@
-// 80pct
 #include<iostream>
 using namespace std;
-void dublare1(int &n) {
-    int invers=0,c;
-    while (n > 0) {
-        c = n % 10;
-        n = n / 10;
-        invers = invers * 10 + c;
-    }  invers = invers * 10 + c;
-    while (invers > 0) {
-        c = invers % 10;
-        invers = invers / 10;
-        n = n * 10 + c;
-    } 
+void dublare1(int &n){
+    int p=1, temp=n;
+    while(temp >= 10){
+        temp /= 10;
+        p *= 10;
+    } p *= 10; n = (temp * p) + n; 
 }
-
 int main() {
     int n;
     cin>>n;
