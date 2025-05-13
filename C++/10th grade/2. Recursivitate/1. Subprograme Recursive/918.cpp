cd983f79168c25e4690dfcd742e2adbcffb669c8
23ovii <danyovidiu21@gmail.com>
2025-05-13 21:13:10 +0300

    solved

--- C++/10th grade/2. Recursivitate/1. Subprograme Recursive/918.cpp
+++ C++/10th grade/2. Recursivitate/1. Subprograme Recursive/918.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 void sumcif(int n, int &s){
-    if(n == 0) return;
-    s += n%10;
-    sumcif(n/10, s);
+    if(n==0){
+        s = 0;
+    } else {
+        sumcif(n/10, s);
+        s += n%10;
+    }
 }
 int main() {
     ios_base::sync_with_stdio(false);
