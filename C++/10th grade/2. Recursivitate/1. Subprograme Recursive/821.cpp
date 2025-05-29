183334e516b30e7068cfc7b8b052e34abf575a88
23ovii <danyovidiu21@gmail.com>
2025-05-29 21:08:13 +0300

    solved

--- C++/10th grade/2. Recursivitate/1. Subprograme Recursive/821.cpp
+++ C++/10th grade/2. Recursivitate/1. Subprograme Recursive/821.cpp
@@ -2,10 +2,7 @@
 using namespace std;
 int cmmdc(int a, int b){
     if(b==0) return a;
-    else{
-        return cmmdc(b, a%b);
-    }
-
+    else return cmmdc(b, a%b);
 }
 int main() {
     ios_base::sync_with_stdio(false);
