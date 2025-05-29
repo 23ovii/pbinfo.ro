dc7721961167f6f6329707d454d595e624f82df3
23ovii <danyovidiu21@gmail.com>
2025-05-29 21:07:34 +0300

    solved

---
+++ C++/10th grade/2. Recursivitate/1. Subprograme Recursive/821.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int cmmdc(int a, int b){
+    if(b==0) return a;
+    else{
+        return cmmdc(b, a%b);
+    }
+
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int a,b; cin>>a>>b;
+    cout<<cmmdc(a,b);
+    return 0;
+}
