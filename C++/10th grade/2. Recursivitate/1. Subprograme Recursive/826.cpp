9e217305f0a85dfeec3839219a87ba4787946132
23ovii <danyovidiu21@gmail.com>
2025-06-04 17:29:12 +0300

    solved

---
+++ C++/10th grade/2. Recursivitate/1. Subprograme Recursive/826.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int cifminpar(int n) {
+    if (n == 0) return 10;
+    int temp = cifminpar(n / 10);
+    if ((n % 10) % 2 == 0) {
+        if (temp < n % 10) return temp;
+        else return n % 10;
+    } else {
+        return temp;
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    int n; cin>>n;
+    cout<<cifminpar(n);
+    return 0;
+}
