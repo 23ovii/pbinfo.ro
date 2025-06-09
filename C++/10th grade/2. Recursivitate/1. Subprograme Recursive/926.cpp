27c8f65174dec0eb01e491de60fa4c50c9c9743e
23ovii <danyovidiu21@gmail.com>
2025-06-09 16:44:42 +0300

    solved

--- C++/10th grade/2. Recursivitate/1. Subprograme Recursive/926.cpp
+++ C++/10th grade/2. Recursivitate/1. Subprograme Recursive/926.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
-int suma(int v[], int n){
+int sum3(int v[], int n){
     if(n==0) return 0;
     else if((v[n-1])%3==0){
-        return v[n-1] + suma(v, n-1);
-    } return suma(v, n-1);
+        return v[n-1] + sum3(v, n-1);
+    } return sum3(v, n-1);
 }
 int main() {
     ios_base::sync_with_stdio(false);
@@ -12,6 +12,6 @@ int main() {
     
     int v[101], n; cin>>n;
     for(int i=0;i<n;i++) cin>>v[i];
-    cout<<suma(v, n);
+    cout<<sum3(v, n);
     return 0;
 }
