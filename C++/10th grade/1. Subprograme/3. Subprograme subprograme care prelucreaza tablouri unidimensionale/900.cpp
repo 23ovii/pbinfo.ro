6e52bd845d87e7e863ed19225cb7c2c9ac9f5a9f
23ovii <danyovidiu21@gmail.com>
2025-06-26 19:26:56 +0300

    solved

--- C++/10th grade/1. Subprograme/3. Subprograme subprograme care prelucreaza tablouri unidimensionale/900.cpp
+++ C++/10th grade/1. Subprograme/3. Subprograme subprograme care prelucreaza tablouri unidimensionale/900.cpp
@@ -3,7 +3,7 @@ using namespace std;
 void ordonare(int *a, int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i-1;j++){
-            if(a[j]>a[j+1]){
+            if(a[j]<a[j+1]){
                 int temp=a[j];
                 a[j]=a[j+1];
                 a[j+1]=temp;
