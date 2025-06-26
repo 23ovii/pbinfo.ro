4d88d49465eb079721226a0226c19a0823dfd5c6
23ovii <danyovidiu21@gmail.com>
2025-06-26 19:23:20 +0300

    solved

---
+++ C++/10th grade/1. Subprograme/3. Subprograme subprograme care prelucreaza tablouri unidimensionale/899.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+void ordonare(int *a, int n){
+    for(int i=0;i<n-1;i++){
+        for(int j=0;j<n-i-1;j++){
+            if(a[j]>a[j+1]){
+                int temp=a[j];
+                a[j]=a[j+1];
+                a[j+1]=temp;
+            }
+        }
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    return 0;
+}
