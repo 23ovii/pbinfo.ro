9fb9dbd2eeedb1523de16c771f9da206d3e58646
ovii <168116208+23ovii@users.noreply.github.com>
2025-11-29 11:39:27 +0200

    Refactor solved.js to use CommonJS module syntax

--- api/api/solved.js
+++ api/api/solved.js
@@ -1,17 +1,13 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-export const config = {
-  runtime: 'nodejs'
-};
 
 module.exports = async (req, res) => {
   try {
     const { data } = await axios.get('https://www.pbinfo.ro/profil/23ovii/probleme');
     const $ = cheerio.load(data);
-
     const solvedText = $('a[href="/profil/23ovii/probleme"]').text();
     const solvedNumber = solvedText.match(/\d+/)?.[0] || '0';
-
+    
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json({
       schemaVersion: 1,
